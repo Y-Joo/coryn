be5656747bf1c378e7398fa0d8118c7174a8094e
DESKTOP-T54RI95\mycom <wldudwn99@gmail.com>
2021-09-07 18:19:49 +0900

    clean

--- proj-coin/backend/core/modules/batch/request_parser.py
+++
@@ -1,3 +0,0 @@
-def parse_request(parameter):
-    url = 'http://127.0.0.1:8000/api/v1/coin/' + parameter
-    return url
--- proj-coin/backend/news/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models import CoinNews
-
-
-class CoinNewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CoinNews
-        fields = '__all__'
